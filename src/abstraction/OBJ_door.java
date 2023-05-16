c3f3394c26ef913aba467092916892d85d8fe925
Tom <lamberttom@cy-tech.fr>
2023-05-16 11:14:56 +0200

    [feature] object bonus key and door

---
+++ src/abstraction/OBJ_door.java
@@ -0,0 +1,14 @@
+package abstraction;
+
+import javafx.scene.image.Image;
+
+import static view.GamePanel.TILE_SIZE;
+
+public class OBJ_door extends SuperObject{
+
+    public OBJ_door(){
+        name = "Key";
+        image = new Image("file:res/bonus/door.png", TILE_SIZE, TILE_SIZE, false, false);
+    }
+
+}
