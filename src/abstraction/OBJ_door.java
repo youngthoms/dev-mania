0d579ae8368d0f88c489c4bc71bf02e22dc34b1c
Tom <lamberttom@cy-tech.fr>
2023-05-21 18:12:57 +0200

    fix colision name

--- src/abstraction/OBJ_door.java
+++ src/abstraction/OBJ_door.java
@@ -9,7 +9,7 @@ public class OBJ_door extends SuperObject {
     public OBJ_door() {
         setName("Door");
         setImage(new Image("res/bonus/door.png", TILE_SIZE, TILE_SIZE, false, false));
-        setColision(true);
+        setCollision(true);
     }
 
 }
